124e66cdae2d2d70fa1018915a001067ba074eaf
Filippo De Luca <filippo.deluca@ovoenergy.com>
2018-08-24 09:45:15 +0200

    Remove unnecessary code

--- src/main/scala/com/github/filosganga/avro/playground/AvroCodec.scala
+++ src/main/scala/com/github/filosganga/avro/playground/AvroCodec.scala
@@ -1,83 +1,28 @@
 package com.github.filosganga.avro.playground
 import com.sksamuel.avro4s._
-import org.apache.avro.generic.{GenericRecord, GenericRecordBuilder}
+import org.apache.avro.generic.GenericRecord
 import org.apache.avro.{Schema, SchemaBuilder}
 
 trait AvroCodec {
 
-  implicit lazy val pizzaSchemaFor: SchemaFor[Pizza] = new SchemaFor[Pizza] {
-
-    override def apply(): Schema =
-      SchemaBuilder
-        .record(classOf[Pizza].getName)
-        .fields()
-        .requiredInt("size")
-        .requiredString("flavour")
-        .endRecord()
-  }
-
-  implicit lazy val pizzaToRecord: ToRecord[Pizza] = new ToRecord[Pizza] {
-    override def apply(t: Pizza): GenericRecord =
-      new GenericRecordBuilder(pizzaSchemaFor())
-        .set("size", t.size)
-        .set("flavour", t.flavour)
-        .build()
-  }
-
-  implicit lazy val pizzaFromRecord: FromRecord[Pizza] = new FromRecord[Pizza] {
-    override def apply(record: GenericRecord): Pizza =
-      Pizza(size = record.get("size").asInstanceOf[Integer], flavour = record.get("flavour").toString)
-
-  }
-
-  implicit lazy val pastaSchemaFor: SchemaFor[Pasta] = new SchemaFor[Pasta] {
-
-    override def apply(): Schema =
-      SchemaBuilder
-        .record(classOf[Pasta].getName)
-        .fields()
-        .requiredInt("weight")
-        .requiredString("format")
-        .requiredString("seasoning")
-        .endRecord()
-  }
-
-  implicit lazy val pastaToRecord: ToRecord[Pasta] = new ToRecord[Pasta] {
-    override def apply(t: Pasta): GenericRecord =
-      new GenericRecordBuilder(pastaSchemaFor())
-        .set("weight", t.weight)
-        .set("format", t.format)
-        .set("seasoning", t.seasoning)
-        .build()
-  }
-
-  implicit lazy val pastaFromRecord: FromRecord[Pasta] = new FromRecord[Pasta] {
-    override def apply(record: GenericRecord): Pasta =
-      Pasta(
-        weight = record.get("weight").asInstanceOf[Integer],
-        format = record.get("format").toString,
-        seasoning = record.get("seasoning").toString
-      )
-  }
-
-  implicit lazy val dishToSchema: ToSchema[Dish] = new ToSchema[Dish] {
+  implicit def dishToSchema(implicit pastaSchemaFor: SchemaFor[Pasta], pizzaSchemaFor: SchemaFor[Pizza]): ToSchema[Dish] = new ToSchema[Dish] {
     override protected val schema: Schema = SchemaBuilder
       .unionOf()
-      .`type`(SchemaFor[Pizza]())
+      .`type`(pizzaSchemaFor())
       .and()
-      .`type`(SchemaFor[Pasta]())
+      .`type`(pastaSchemaFor())
       .endUnion()
   }
 
 
-  implicit lazy val dishToValue: ToValue[Dish] = new ToValue[Dish] {
+  implicit def dishToValue(implicit pastaToRecord: ToRecord[Pasta], pizzaToRecord: ToRecord[Pizza]): ToValue[Dish] = new ToValue[Dish] {
     override def apply(value: Dish): Any = value match {
       case p: Pasta => pastaToRecord(p)
       case p: Pizza => pizzaToRecord(p)
     }
   }
 
-  implicit lazy val dishFromValue: FromValue[Dish] = new FromValue[Dish] {
+  implicit def dishFromValue(implicit pastaSchemaFor: SchemaFor[Pasta], pastaFromRecord: FromRecord[Pasta], pizzaFromRecord: FromRecord[Pizza], pizzaSchemaFor: SchemaFor[Pizza]): FromValue[Dish] = new FromValue[Dish] {
     private val PizzaSchema = pizzaSchemaFor()
     private val PastaSchema = pastaSchemaFor()
 
